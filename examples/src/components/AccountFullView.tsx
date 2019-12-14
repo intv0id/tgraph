3ee089aa0e7184b9f8608ba006c80d743acfad49
Clément Trassoudaine <clement.trassoudaine@gmail.com>
2019-12-14 22:28:00 +0100

    Github example display enhancement

--- examples/src/components/AccountFullView.tsx
+++ examples/src/components/AccountFullView.tsx
@@ -12,11 +12,44 @@ export interface IAccountFullViewProps {
 export class AccountFullView extends Component<IAccountFullViewProps, IAccountFullViewState>{
     render() {
         return <div className="AccountFullView">
-            <h2 className="accountLogin">{this.props.account.name}</h2>
-            <br/>
-            <img src={this.props.account.data.avatar_url}/>
-            <br/>
-            <p>{JSON.stringify(this.props.account.data)}</p>
+            <h2 className="accountLogin">
+                <a href={this.props.account.data.html_url}>{this.props.account.name}</a>
+                <br />
+                {
+                    this.props.account.data.email ?
+                        <a href={`mailto://${this.props.account.data.email}`}>📧</a> :
+                        null
+                }
+            </h2>
+            <img className="avatar" src={this.props.account.data.avatar_url} />
+            {
+                this.props.account.data.name ?
+                    <p><b>Name : </b>{this.props.account.data.name}</p> :
+                    null
+            }
+            {
+                this.props.account.data.company ?
+                    <p><b>Company : </b>{this.props.account.data.company}</p> :
+                    null
+            }
+            {
+                this.props.account.data.blog ?
+                    <p><b>Blog : </b><a href={this.props.account.data.blog}>{this.props.account.data.blog}</a></p> :
+                    null
+            }
+            {
+                this.props.account.data.location ?
+                    <p><b>Location : </b>{this.props.account.data.location}</p> :
+                    null
+            }
+            <p><q>{this.props.account.data.bio}</q></p>
+            <p>
+                {this.props.account.data.followers} 🔼
+                {"   "}
+                {this.props.account.data.following} 🔽
+                {"   "}
+                {this.props.account.data.public_repos} 📚
+            </p>
         </div>
     }
 }
